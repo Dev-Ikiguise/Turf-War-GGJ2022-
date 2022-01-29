f86619ea29c4ad5298d84e2c377609d949aec328
WEBSTERAD\WUStudent <dts@webster.edu>
2022-01-28 21:07:21 -0600

    did it

--- Turf War GGJ2022/Assets/Code/MovementVerifier.cs
+++ Turf War GGJ2022/Assets/Code/MovementVerifier.cs
@@ -6,25 +6,65 @@ public class MovementVerifier : MonoBehaviour
 {
     Movement2 player;
 
+    public enum Direction
+    {
+        up,
+        down,
+        left,
+        right
+    }
+
+    public Direction currentDirection;
+
     void Start()
     {
         player = GetComponentInParent<Movement2>();
         print(player);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<GridBlock>())
-        {
-            player.canMoveForward = other.GetComponent<GridBlock>().canMoveInto;
-        }
-    }
-
     private void OnTriggerStay(Collider other)
     {
         if (other.GetComponent<GridBlock>())
         {
-            player.canMoveForward = other.GetComponent<GridBlock>().canMoveInto;
+            if (currentDirection == Direction.up)
+            {
+                player.canMoveUp = false;
+            }
+            else if (currentDirection == Direction.down)
+            {
+                player.canMoveDown = false;
+            }
+            else if (currentDirection == Direction.left)
+            {
+                player.canMoveLeft = false;
+            }
+            else if (currentDirection == Direction.right)
+            {
+                player.canMoveRight = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<GridBlock>())
+        {
+            if (currentDirection == Direction.up)
+            {
+                player.canMoveUp = true;
+            }
+            else if (currentDirection == Direction.down)
+            {
+                player.canMoveDown = true;
+            }
+            else if (currentDirection == Direction.left)
+            {
+                player.canMoveLeft = true;
+            }
+            else if (currentDirection == Direction.right)
+            {
+                player.canMoveRight = true;
+            }
         }
     }
 }
